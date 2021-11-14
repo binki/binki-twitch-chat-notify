99b8adb41184ffc32db6e87301b1d23bdab4e532
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2021-11-14 14:48:16 -0500

    Support timestamps being disabled.
    Fixes #3.

--- binki-twitch-chat-notify.user.js
+++ binki-twitch-chat-notify.user.js
@@ -39,7 +39,7 @@
           case 'childList': {
             const lineElement = target.lastChild;
             const currentText = lineElement.textContent;
-            if (currentText !== lastSeenText) {
+            if (lineElement.matches('.chat-line__message') && currentText !== lastSeenText) {
               lastSeenText = currentText;
               // Some people might claim that document.hidden is sufficient. However, with my workflow,
               // I never minimize windows even if they are not actually visible to me. So I need to use
@@ -47,29 +47,27 @@
               // is available, let me know!
               if (window.document.hidden || isBlurred) {
                 // Extract slightly nicer formatted message.
-                const timeStampElement = lineElement.querySelector('[data-test-selector=chat-timestamp]');
-                // If the timeStampElement doesn’t exist, it probably isn’t a message.
-                if (timeStampElement) {
-                  const timeStamp = timeStampElement.textContent;
-                  // has srcset which we could parse instead of using src
-                  const badgeElement = lineElement.querySelector('img.chat-badge');
-                  const badgeSrc = badgeElement === null ? undefined : badgeElement.src;
-                  const userName = lineElement.querySelector('[data-test-selector=message-username]').textContent;
-                  const chatMessageSeparatorElement = lineElement.querySelector('[data-test-selector=chat-message-separator]');
-                  const messageText = (function concatMessage(currentElement) {
-                    if (currentElement === null) return '';
-                    return (() => {
-                      const foundImage = currentElement.querySelector('img.chat-image');
-                      return foundImage ? ` ${foundImage.alt} ` : currentElement.textContent;
-                    })() + concatMessage(currentElement.nextSibling);
-                  })(chatMessageSeparatorElement.nextSibling);
-                  const avatarElement = document.querySelector('.channel-info-content .tw-image-avatar, .mosaic-root .tw-image-avatar');
-                  const avatarSrc = avatarElement === null ? undefined : avatarElement.src;
-                  const notification = new window.Notification(`${timeStamp} <${userName}> ${messageText}`, {
-                    image: badgeSrc,
-                    icon: avatarSrc,
-                  });
-                }
+                const maybeTimeStampElement = lineElement.querySelector('[data-test-selector=chat-timestamp]');
+                // If the timeStampElement doesn’t exist, the user probably disabled it (issue #3).
+                const maybeTimeStamp = maybeTimeStampElement ? maybeTimeStampElement.textContent : null;
+                // has srcset which we could parse instead of using src
+                const badgeElement = lineElement.querySelector('img.chat-badge');
+                const badgeSrc = badgeElement === null ? undefined : badgeElement.src;
+                const userName = lineElement.querySelector('[data-test-selector=message-username]').textContent;
+                const chatMessageSeparatorElement = lineElement.querySelector('[data-test-selector=chat-message-separator]');
+                const messageText = (function concatMessage(currentElement) {
+                  if (currentElement === null) return '';
+                  return (() => {
+                    const foundImage = currentElement.querySelector('img.chat-image');
+                    return foundImage ? ` ${foundImage.alt} ` : currentElement.textContent;
+                  })() + concatMessage(currentElement.nextSibling);
+                })(chatMessageSeparatorElement.nextSibling);
+                const avatarElement = document.querySelector('.channel-info-content .tw-image-avatar, .mosaic-root .tw-image-avatar');
+                const avatarSrc = avatarElement === null ? undefined : avatarElement.src;
+                const notification = new window.Notification(`${maybeTimeStamp ? maybeTimeStamp + ' ' : ''}<${userName}> ${messageText}`, {
+                  image: badgeSrc,
+                  icon: avatarSrc,
+                });
               }
             }
             break;
